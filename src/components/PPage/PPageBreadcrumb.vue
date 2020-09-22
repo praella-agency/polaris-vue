3c8d0e8ed2faa28a26561f9b770b3c124624a8d7
Shailesh Desai <Shailesh@planetx.in>
2020-09-22 16:33:36 +0530

    Change svg to icon component

--- src/components/PPage/PPageBreadcrumb.vue
+++ src/components/PPage/PPageBreadcrumb.vue
@@ -6,11 +6,7 @@
                     <a v-for="breadcrumb in breadcrumbs" class="Polaris-Breadcrumbs__Breadcrumb" :href="breadcrumb.url" data-polaris-unstyled="true">
                         <span class="Polaris-Breadcrumbs__ContentWrapper">
                             <span class="Polaris-Breadcrumbs__Icon">
-                                <span class="Polaris-Icon">
-                                    <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
-                                      <path d="M12 16a.997.997 0 0 1-.707-.293l-5-5a.999.999 0 0 1 0-1.414l5-5a.999.999 0 1 1 1.414 1.414L8.414 10l4.293 4.293A.999.999 0 0 1 12 16" fill-rule="evenodd"></path>
-                                    </svg>
-                                </span>
+                                <PIcon source="ChevronLeftMinor" />
                             </span>
                             <span class="Polaris-Breadcrumbs__Content">{{ breadcrumb.content }}</span>
                         </span>
@@ -24,9 +20,9 @@
 <script lang="ts">
   import { Component, Vue, Prop } from 'vue-property-decorator';
   import { classNames, variationName } from '@/utilities/css';
-
+  import { PIcon } from '@/components/PIcon';
   @Component({
-    components: { },
+    components: { PIcon },
   })
   export default class PPageBreadcrumb extends Vue {
     @Prop(Object) public breadcrumbs!: object;
