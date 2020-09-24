84095995c3a24b50ea38f039a161ca8e6b6d90bc
Divyank <divyank@hulkapps.com>
2020-09-24 11:29:45 +0530

    PResourceList

--- src/components/PResourceList/PResourceList.vue
+++ src/components/PResourceList/PResourceList.vue
@@ -1,30 +1,42 @@
 <template>
 
     <div :class="className">
-        <div class="Polaris-ResourceList__FiltersWrapper">
+        <div class="Polaris-ResourceList__FiltersWrapper" v-if="$slots.filter">
             <slot name="filter" />
         </div>
 
-        <div class="Polaris-EmptyState Polaris-EmptyState--withinContentContainer">
+        <!--<div class="Polaris-EmptyState Polaris-EmptyState&#45;&#45;withinContentContainer">
             <div class="Polaris-EmptyState__Section">
                 <div class="Polaris-EmptyState__DetailsContainer">
                     <div class="Polaris-EmptyState__Details">
                         <div class="Polaris-TextContainer">
-                            <p class="Polaris-DisplayText Polaris-DisplayText--sizeSmall">Upload a file to get started</p>
+                            <p class="Polaris-DisplayText Polaris-DisplayText&#45;&#45;sizeSmall">Upload a file to get started</p>
                             <div class="Polaris-EmptyState__Content">
                                 <p>You can use the Files section to upload images, videos, and other documents</p>
                             </div>
                         </div>
                         <div class="Polaris-EmptyState__Actions">
-                            <div class="Polaris-Stack Polaris-Stack--alignmentCenter">
-                                <div class="Polaris-Stack__Item"><button type="button" class="Polaris-Button Polaris-Button--primary"><span class="Polaris-Button__Content"><span class="Polaris-Button__Text">Upload files</span></span></button></div>
+                            <div class="Polaris-Stack Polaris-Stack&#45;&#45;alignmentCenter">
+                                <div class="Polaris-Stack__Item"><button type="button" class="Polaris-Button Polaris-Button&#45;&#45;primary"><span class="Polaris-Button__Content"><span class="Polaris-Button__Text">Upload files</span></span></button></div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="Polaris-EmptyState__ImageContainer"><img src="https://cdn.shopify.com/s/files/1/2376/3301/products/emptystate-files.png" role="presentation" alt="" class="Polaris-EmptyState__Image"></div>
             </div>
-        </div>
+        </div>-->
+
+        <PResourceListHeader
+                v-bind="$attrs"
+                :selectable="selectable"
+                :selectedItems="computedValue"
+                :checked="checked"
+                :checkedAll="checkedAll"
+                :count="count"
+                v-on="$listeners"
+                @toggle-all="onToggledAll($event)" />
+
+
     </div>
 </template>
 
@@ -33,39 +45,59 @@
     import { Component, Vue, Prop } from 'vue-property-decorator';
     import { classNames, variationName } from '@/utilities/css';
     import {PImage} from '@/components/PImage';
-
-    type Size = 'small' | 'medium' | 'large';
-    enum Status {
-        Pending = 'PENDING',
-        Loaded = 'LOADED',
-        Errored = 'ERRORED',
-    }
+    import PResourceListHeader from '@/components/PResourceList/PResourceListHeader.vue';
 
     @Component({
         components: {
+            PResourceListHeader,
             PImage,
         },
     })
     export default class PResourceList extends Vue {
 
-        @Prop({type: String, default: 'medium'}) public size!: Size;
-        @Prop(Boolean) public customer!: boolean;
-        @Prop(Object) public resourceName!: object;
-        @Prop(String) public initials!: string;
         @Prop(String) public source!: string;
+        @Prop(Array) public selected!: number[];
+        @Prop(Boolean) public selectable!: boolean;
 
-        public status = Status.Pending;
+        public selectedItems = this.selected ? this.selected : [];
 
+        public get count() {
+
+            return this.$slots.default ? this.$slots.default.length : 0;
+        }
 
         public get className() {
             return classNames(
-                'Polaris-ResourceList__ResourceListWrapper'
+                'Polaris-ResourceList__ResourceListWrapper',
             );
         }
 
-        public styleClass(name?: string) {
-            const finalStyleClasses = ['one', 'two', 'three', 'four', 'five', 'six'];
-            return name ? finalStyleClasses[name.charCodeAt(0) % finalStyleClasses.length] : finalStyleClasses[0];
+        public get checkedAll() {
+
+            return this.$slots.default && this.$slots.default.length === this.selected.length;
+        }
+
+        public get checked() {
+
+            return this.selected.length > 0;
+        }
+
+        public get computedValue() {
+            return this.selectedItems;
+        }
+
+        public set computedValue(selected: any[]) {
+            this.selectedItems = selected;
+            this.$emit('change', selected);
+        }
+
+        public onToggledAll(checked) {
+
+            if (checked) {
+                this.computedValue = []
+            } else {
+                this.computedValue = [1,2,3]
+            }
         }
     }
 </script>
--- src/components/PResourceList/PResourceListHeader.vue
+++ src/components/PResourceList/PResourceListHeader.vue
@@ -2,160 +2,130 @@
 
     <div :class="className">
         <div class="Polaris-ResourceList__HeaderContentWrapper">
-            <div class="Polaris-ResourceList__HeaderTitleWrapper">Showing {{resourceTitle}}</div>
-            <div class="Polaris-ResourceList__CheckableButtonWrapper">
-                <div class="Polaris-ResourceList-CheckableButton Polaris-ResourceList-CheckableButton__CheckableButton--plain">
-                    <div class="Polaris-ResourceList-CheckableButton__Checkbox">
-                        <label class="Polaris-Choice Polaris-Choice--labelHidden" for="PolarisCheckbox7">
-                                <span class="Polaris-Choice__Control">
-                                    <span class="Polaris-Checkbox">
-                                        <input id="PolarisCheckbox7" type="checkbox" class="Polaris-Checkbox__Input" aria-invalid="false" role="checkbox" aria-checked="false" value="">
-                                        <span class="Polaris-Checkbox__Backdrop"></span>
-                                        <span class="Polaris-Checkbox__Icon">
-                                            <span class="Polaris-Icon">
-                                                <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
-                                                    <path d="M8.315 13.859l-3.182-3.417a.506.506 0 0 1 0-.684l.643-.683a.437.437 0 0 1 .642 0l2.22 2.393 4.942-5.327a.437.437 0 0 1 .643 0l.643.684a.504.504 0 0 1 0 .683l-5.91 6.35a.437.437 0 0 1-.642 0"></path>
-                                                </svg>
-                                            </span>
-                                        </span>
-                                    </span>
-                                </span>
-                            <span class="Polaris-Choice__Label">Select all 2 customers</span>
-                        </label>
-                    </div>
-                    <span class="Polaris-ResourceList-CheckableButton__Label">Showing 2 customers</span>
-                </div>
-            </div>
-            <div class="Polaris-ResourceList__SortWrapper">
-                <div class="Polaris-Labelled--hidden">
-                    <div class="Polaris-Labelled__LabelWrapper">
-                        <div class="Polaris-Label"><label id="PolarisSelect1Label" for="PolarisSelect1" class="Polaris-Label__Text">Sort by</label></div>
-                    </div>
-                    <div class="Polaris-Select">
-                        <select id="PolarisSelect1" class="Polaris-Select__Input" aria-invalid="false">
-                            <option value="DATE_MODIFIED_DESC">Newest update</option>
-                            <option value="DATE_MODIFIED_ASC">Oldest update</option>
-                        </select>
-                        <div class="Polaris-Select__Content" aria-hidden="true" aria-disabled="false">
-                            <span class="Polaris-Select__InlineLabel">Sort by</span>
-                            <span class="Polaris-Select__SelectedOption">Newest update</span>
-                            <span class="Polaris-Select__Icon"><span class="Polaris-Icon">
-                                    <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
-                                        <path d="M13 8l-3-3-3 3h6zm-.1 4L10 14.9 7.1 12h5.8z" fill-rule="evenodd"></path>
-                                    </svg>
-                                </span>
-                                </span>
-                        </div>
-                        <div class="Polaris-Select__Backdrop"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="Polaris-ResourceList__SelectButtonWrapper">
-                <button type="button" class="Polaris-Button">
-                        <span class="Polaris-Button__Content">
-                            <span class="Polaris-Button__Icon">
-                                <span class="Polaris-Icon">
-                                    <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
-                                        <path d="M9 14.41l-3.71-3.7 1.42-1.42L9 11.59l5.29-5.3 1.42 1.42L9 14.41zM17 2H3c-.55 0-1 .45-1 1v14c0 .55.45 1 1 1h14c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1z"></path>
-                                    </svg>
-                                </span>
-                            </span>
-                            <span class="Polaris-Button__Text">Select</span>
-                        </span>
-                </button>
+            <div class="Polaris-ResourceList__HeaderTitleWrapper" v-if="false">{{resourceTitle}}</div>
+            <div class="Polaris-ResourceList__CheckableButtonWrapper" v-if="selectedItems">
+                <PCheckableButton plain v-bind="$attrs" v-on="$listeners"></PCheckableButton>
             </div>
         </div>
-        <div class="Polaris-ResourceList__BulkActionsWrapper">
+
+        <div class="Polaris-ResourceList__BulkActionsWrapper" v-if="true">
+            <div class="Polaris-ResourceList-BulkActions__Group Polaris-ResourceList-BulkActions__Group--largeScreen Polaris-ResourceList-BulkActions__Group--entered">
+                <div class="Polaris-ResourceList-BulkActions__ButtonGroupWrapper">
+                    <PButtonGroup segmented>
+                        <PButton>Test</PButton>
+                        <PButton>Test</PButton>
+                        <PButton>Test</PButton>
+
+                    </PButtonGroup>
+                </div>
+            </div>
+        </div>
+
+        <!--<div class="Polaris-ResourceList__BulkActionsWrapper" v-if="true">
             <div>
-                <div class="Polaris-ResourceList-BulkActions__Group Polaris-ResourceList-BulkActions__Group--largeScreen Polaris-ResourceList-BulkActions__Group--exited">
+                <div class="Polaris-ResourceList-BulkActions__Group Polaris-ResourceList-BulkActions__Group&#45;&#45;largeScreen Polaris-ResourceList-BulkActions__Group&#45;&#45;entered">
                     <div class="Polaris-ResourceList-BulkActions__ButtonGroupWrapper">
-                        <div class="Polaris-ButtonGroup Polaris-ButtonGroup--segmented" data-buttongroup-segmented="true">
+                        <PButtonGroup>
+
+                        </PButtonGroup>
+                        <div class="Polaris-ButtonGroup Polaris-ButtonGroup&#45;&#45;segmented" data-buttongroup-segmented="true">
                             <div class="Polaris-ButtonGroup__Item">
-                                <div class="Polaris-ResourceList-CheckableButton">
+                                <div class="Polaris-ResourceList-CheckableButton Polaris-ResourceList-CheckableButton__CheckableButton&#45;&#45;selectMode Polaris-ResourceList-CheckableButton__CheckableButton&#45;&#45;selected">
                                     <div class="Polaris-ResourceList-CheckableButton__Checkbox">
-                                        <label class="Polaris-Choice Polaris-Choice--labelHidden" for="PolarisCheckbox8">
-                                                <span class="Polaris-Choice__Control">
-                                                    <span class="Polaris-Checkbox">
-                                                        <input id="PolarisCheckbox8" type="checkbox" class="Polaris-Checkbox__Input" aria-invalid="false" role="checkbox" aria-checked="false" value="">
-                                                        <span class="Polaris-Checkbox__Backdrop"></span>
-                                                        <span class="Polaris-Checkbox__Icon">
-                                                            <span class="Polaris-Icon">
-                                                                <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
-                                                                    <path d="M8.315 13.859l-3.182-3.417a.506.506 0 0 1 0-.684l.643-.683a.437.437 0 0 1 .642 0l2.22 2.393 4.942-5.327a.437.437 0 0 1 .643 0l.643.684a.504.504 0 0 1 0 .683l-5.91 6.35a.437.437 0 0 1-.642 0"></path>
-                                                                </svg>
-                                                            </span>
-                                                        </span>
+                                        <label class="Polaris-Choice Polaris-Choice&#45;&#45;labelHidden" for="PolarisCheckbox2">
+                                        <span class="Polaris-Choice__Control">
+                                            <span class="Polaris-Checkbox">
+                                                <input id="PolarisCheckbox2" type="checkbox" class="Polaris-Checkbox__Input" aria-invalid="false" role="checkbox" aria-checked="true" value="">
+                                                <span class="Polaris-Checkbox__Backdrop"></span>
+                                                <span class="Polaris-Checkbox__Icon">
+                                                    <span class="Polaris-Icon">
+                                                        <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
+                                                            <path d="M8.315 13.859l-3.182-3.417a.506.506 0 0 1 0-.684l.643-.683a.437.437 0 0 1 .642 0l2.22 2.393 4.942-5.327a.437.437 0 0 1 .643 0l.643.684a.504.504 0 0 1 0 .683l-5.91 6.35a.437.437 0 0 1-.642 0"></path>
+                                                        </svg>
                                                     </span>
                                                 </span>
-                                            <span class="Polaris-Choice__Label">Select all 2 customers</span>
+                                            </span>
+                                        </span>
+                                        <span class="Polaris-Choice__Label">Deselect all 2 customers</span>
                                         </label>
                                     </div>
-                                    <span class="Polaris-ResourceList-CheckableButton__Label">0 selected</span>
+                                    <span class="Polaris-ResourceList-CheckableButton__Label">2 selected</span>
                                 </div>
                             </div>
                             <div class="Polaris-ButtonGroup__Item">
                                 <div class="Polaris-ResourceList-BulkActions__BulkActionButton">
                                     <button type="button" class="Polaris-Button">
-                                            <span class="Polaris-Button__Content">
-                                                <span class="Polaris-Button__Text">Edit customers</span>
-                                            </span>
+									<span class="Polaris-Button__Content">
+										<span class="Polaris-Button__Text">Edit customers</span>
+									</span>
                                     </button>
                                 </div>
                             </div>
-                            <div class="Polaris-ButtonGroup__Item"><div>
+                            <div class="Polaris-ButtonGroup__Item">
                                 <div>
-                                    <div class="Polaris-ResourceList-BulkActions__BulkActionButton">
-                                        <button type="button" class="Polaris-Button" tabindex="0" aria-controls="Polarispopover8" aria-owns="Polarispopover8" aria-expanded="false">
-                                                <span class="Polaris-Button__Content">
-                                                    <span class="Polaris-Button__Text">More actions</span>
-                                                    <span class="Polaris-Button__Icon">
-                                                        <div class="Polaris-Button__DisclosureIcon">
-                                                            <span class="Polaris-Icon">
-                                                                <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
-                                                                    <path d="M5 8l5 5 5-5z" fill-rule="evenodd"></path></svg>
-                                                            </span>
-                                                        </div>
-                                                    </span>
-                                                </span>
-                                        </button>
+                                    <div>
+                                        <div class="Polaris-ResourceList-BulkActions__BulkActionButton">
+                                            <button type="button" class="Polaris-Button" tabindex="0" aria-controls="Polarispopover6" aria-owns="Polarispopover6" aria-expanded="false">
+											<span class="Polaris-Button__Content">
+												<span class="Polaris-Button__Text">More actions</span>
+												<span class="Polaris-Button__Icon">
+													<div class="Polaris-Button__DisclosureIcon">
+														<span class="Polaris-Icon">
+															<svg viewBox="0 0 20 20" class="Polaris-Icon__Svg" focusable="false" aria-hidden="true">
+																<path d="M5 8l5 5 5-5z" fill-rule="evenodd"></path>
+															</svg>
+														</span>
+													</div>
+												</span>
+											</span>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div>-->
+
+
     </div>
 </template>
 
 <script lang="ts">
 
-    import { Component, Vue, Prop } from 'vue-property-decorator';
+    import { Component, Prop, Vue } from 'vue-property-decorator';
     import { classNames } from '@/utilities/css';
+    import { PCheckableButton } from '@/components/PCheckableButton';
+    import { PButtonGroup } from '@/components/PButtonGroup';
+    import {PButton} from "@/components/PButton";
 
     interface ResourceNameInterface {
-        singular: string,
-        plural: string,
+        singular: string;
+        plural: string;
     }
 
     @Component({
         components: {
-
+            PCheckableButton, PButtonGroup, PButton
         },
     })
-    export default class PResourceList extends Vue {
+    export default class PResourceListHeader extends Vue {
 
-        @Prop({required: true,type: Object}) public resourceName!: ResourceNameInterface;
+        @Prop({required: true, type: Object}) public resourceName!: ResourceNameInterface;
         @Prop(Object) public promotedBulkActions!: object;
         @Prop(Object) public BulkActions!: object;
         @Prop(Number) public totalCount!: number;
+        @Prop(Array) public selectedItems!: number[];
+        @Prop(Boolean) public selectable!: boolean;
+        @Prop({required: true, type: Number}) public count!: number;
 
         public get className() {
+
             return classNames(
                 'Polaris-ResourceList__HeaderWrapper',
-                this.promotedBulkActions && 'Polaris-ResourceList__HeaderWrapper--hasSelect',
+                this.selectable && 'Polaris-ResourceList__HeaderWrapper--hasSelect Polaris-ResourceList__HeaderWrapper--inSelectMode',
                 this.promotedBulkActions && 'Polaris-ResourceList__HeaderWrapper--hasSort',
             );
         }
@@ -163,9 +133,8 @@
         public get resourceTitle() {
 
             const resourceName: ResourceNameInterface = this.resourceName;
-            const title = this.$slots.hasOwnProperty('default') && (this.$slots.default ?? []).length > 2 ? resourceName.plural : resourceName.singular;
-
-
+            const resource = this.count > 1 ? resourceName.plural : resourceName.singular;
+            return `Showing ${this.count} ${this.totalCount ? ' of ' + this.totalCount : ''} ${resource}`;
         }
     }
 </script>
