5d375cc25d624dd46e08ef3e368bf5973cba28bf
Divyank <divyank@hulkapps.com>
2020-10-07 11:22:08 +0530

    Added PPopoverOverlay

---
+++ src/components/PPopover/components/PPopoverOverlay.vue
@@ -0,0 +1,36 @@
+<template>
+    <PPositionedOverlay
+            :fullWidth="fullWidth"
+            :active="active"
+            :preferredPosition="preferredPosition"
+            :activatorId="activatorId"
+            @scrollout="handleScrollOut">
+        <template slot="overlay" slot-scope="props">
+            <slot name="overlay" :data="props"></slot>
+        </template>
+    </PPositionedOverlay>
+</template>
+
+<script lang="ts">
+
+    import { Component, Vue, Prop } from 'vue-property-decorator';
+    import PPositionedOverlay from "@/components/PPopover/components/PPositionedOverlay.vue";
+
+    @Component({
+        components: { PPositionedOverlay },
+    })
+    export default class PPopoverOverlay extends Vue {
+
+        @Prop(String) public id!: string;
+        @Prop(Boolean) public active!: boolean;
+        @Prop(Boolean) public preventAutofocus!: boolean;
+        @Prop(Boolean) public sectioned!: boolean;
+        @Prop(Boolean) public fullWidth!: boolean;
+        @Prop(String) public preferredPosition!: string;
+        @Prop(String) public activatorId!: string;
+
+        public handleScrollOut() {
+            this.$emit('scrollout');
+        }
+    }
+</script>
