10b880ef741e5e763dfd2b79a0ddcd69e58e05de
Divyank <divyank@hulkapps.com>
2021-04-07 15:04:31 +0530

    Fixed Tab styling

--- src/components/PTabs/PTab.vue
+++ src/components/PTabs/PTab.vue
@@ -1,56 +1,62 @@
 <template>
   <li :class="tabContainerClassNames">
     <button
-      :id="id"
-      type="button"
-      :tabIndex="tabIndex"
-      :class="className"
-      @click="$emit('click', id, $event)"
+        v-if="!to"
+        :id="id"
+        type="button"
+        :tabIndex="tabIndex"
+        :class="className"
+        @click="$emit('click', id, $event)"
     >
       <span :class="tabTitleClassNames"><slot/></span>
     </button>
+
+    <router-link v-else :tabIndex="tabIndex" active-class="Polaris-Tabs__Tab--selected" :class="className" :to="to" >
+      <span :class="tabTitleClassNames"><slot/></span>
+    </router-link>
   </li>
 </template>
 
 <script lang="ts">
-  import { Component, Vue, Prop } from 'vue-property-decorator';
-  import { classNames } from '@/utilities/css';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { classNames } from '@/utilities/css';
 
-  @Component
+@Component
 
-  export default class PTab extends Vue {
+export default class PTab extends Vue {
 
-    @Prop(String) public id!: string;
-    @Prop(Boolean) public focused!: boolean;
-    @Prop(Boolean) public selected!: boolean;
+  @Prop(String) public id!: string;
+  @Prop(Boolean) public focused!: boolean;
+  @Prop(Boolean) public selected!: boolean;
+  @Prop(String) public to!: string;
 
-    public tabIndex: number = -1;
-    public mounted() {
-      this.tabIndex = this.focused ? 0 : -1;
-    }
+  public tabIndex: number = -1;
+  public mounted() {
+    this.tabIndex = this.focused ? 0 : -1;
+  }
 
-    public get tabContainerClassNames() {
-      return classNames(
+  public get tabContainerClassNames() {
+    return classNames(
         'Polaris-Tabs__TabContainer',
-      );
-    }
+    );
+  }
 
-    public get tabTitleClassNames() {
-      return classNames(
+  public get tabTitleClassNames() {
+    return classNames(
         'Polaris-Tabs__Title',
-      );
-    }
+    );
+  }
 
-    public get className() {
-      return classNames(
+  public get className() {
+    return classNames(
         'Polaris-Tabs__Tab',
         this.selected && `Polaris-Tabs__Tab--selected`,
-      );
-    }
-
-    // protected handleClick(id: string) {
-    //
-    //   this.$emit('click', this.$event, id);
-    // }
+    );
   }
+
+  // protected handleClick(id: string) {
+  //
+  //   this.$emit('click', this.$event, id);
+  // }
+}
 </script>
--- src/components/PTabs/PTabs.vue
+++ src/components/PTabs/PTabs.vue
@@ -1,58 +1,67 @@
 <template>
   <div>
-    <div class="Polaris-Tabs__Wrapper">
+    <div :class="wrapperClassName">
       <ul :class="className">
         <PTab v-for="(tab, tabIndex) in tabs"
-             :key="tabIndex + '-' + tab.id"
-             :id="tab.id"
-             :selected="selected === tabIndex"
-             @click="handleTabClick"
+              :key="tabIndex + '-' + tab.id"
+              :id="tab.id"
+              :to="tab.to"
+              :selected="selected === tabIndex"
+              @click="handleTabClick"
         >
           {{tab.content}}
         </PTab>
       </ul>
     </div>
-    <PPanel v-for="(tab, tabIndex) in tabs" :key="tab.id" :id="tab.id + '-panel'" :tabID="tab.id" :hidden="!(selected === tabIndex)"><slot v-if="selected === tabIndex"/></PPanel>
+    <PPanel v-if="!navigation" v-for="(tab, tabIndex) in tabs" :key="tab.id" :id="tab.id + '-panel'" :tabID="tab.id" :hidden="!(selected === tabIndex)"><slot v-if="selected === tabIndex"/></PPanel>
   </div>
 </template>
 
 <script lang="ts">
-  import { Component, Vue, Prop } from 'vue-property-decorator';
-  import { classNames } from '@/utilities/css';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { classNames } from '@/utilities/css';
 
-  import {TabDescriptor} from './types';
+import {TabDescriptor} from './types';
 
-  import PTab from './PTab.vue';
-  import PPanel from './PPanel.vue';
+import PTab from './PTab.vue';
+import PPanel from './PPanel.vue';
 
-  @Component({
-    components: {
-      PTab,
-      PPanel,
-    },
-  })
+@Component({
+  components: {
+    PTab,
+    PPanel,
+  },
+})
 
-  export default class PTabs extends Vue {
+export default class PTabs extends Vue {
 
-    @Prop(Array) public tabs!: TabDescriptor[];
-    @Prop(Number) public selected!: number;
+  @Prop(Array) public tabs!: TabDescriptor[];
+  @Prop(Number) public selected!: number;
+  @Prop(Boolean) public navigation!: boolean;
 
-    public get className() {
-      return classNames(
+  public get className() {
+    return classNames(
         'Polaris-Tabs',
-      );
-    }
-
-    protected handleTabClick(id: string, event: object) {
-
-      const tab = this.tabs.find((aTab) => aTab.id === id);
-      if (tab === null) {
-        return;
-      }
-
-      const selectedIndex = this.tabs.indexOf(tab!);
-
-      this.$emit('select', selectedIndex, event);
-    }
+    );
   }
+
+  public get wrapperClassName() {
+    return classNames(
+        'Polaris-Tabs__Wrapper',
+        this.navigation && 'Polaris-Tabs__Navigation',
+    );
+  }
+
+  protected handleTabClick(id: string, event: object) {
+
+    const tab = this.tabs.find((aTab) => aTab.id === id);
+    if (tab === null) {
+      return;
+    }
+
+    const selectedIndex = this.tabs.indexOf(tab!);
+
+    this.$emit('select', selectedIndex, event);
+  }
+}
 </script>
