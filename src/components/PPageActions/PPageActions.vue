305f9faa10cce710381ab5fa9a883b45d27883db
Divyank <divyank@hulkapps.com>
2021-05-18 12:26:36 +0530

    Added option for secondary page action button

--- src/components/PPageActions/PPageActions.vue
+++ src/components/PPageActions/PPageActions.vue
@@ -1,48 +1,55 @@
 <template>
-    <div class="Polaris-PageActions">
-        <PStack :distribution="distribution" spacing="tight">
-            <PButtonGroup>
-                <PButton v-for="(secondaryAction, secondaryActionIndex) in secondaryActions" :key="`secondaryAction-${secondaryActionIndex}`" @click="secondaryAction.onAction()" v-bind="secondaryAction">
-                    {{secondaryAction.content}}
-                </PButton>
-            </PButtonGroup>
-            <PButton @click="primaryAction.onAction()" v-bind="primaryAction" primary>
-                {{primaryAction.content}}
-            </PButton>
-        </PStack>
-    </div>
+  <div class="Polaris-PageActions">
+    <PStack :distribution="distribution" spacing="tight">
+      <PButtonGroup>
+        <PButton v-for="(secondaryAction, secondaryActionIndex) in secondaryActions"
+                 :key="`secondaryAction-${secondaryActionIndex}`" @click="secondaryAction.onAction()"
+                 v-bind="secondaryAction">
+          {{ secondaryAction.content }}
+        </PButton>
+      </PButtonGroup>
+      <PButtonGroup v-if="secondaryAction">
+        <PButton @click="secondaryAction.onAction()" v-bind="secondaryAction">
+          {{ secondaryAction.content }}
+        </PButton>
+        <PButton @click="primaryAction.onAction()" v-bind="primaryAction" primary>
+          {{ primaryAction.content }}
+        </PButton>
+      </PButtonGroup>
+      <PButton v-else @click="primaryAction.onAction()" v-bind="primaryAction" primary>
+        {{ primaryAction.content }}
+      </PButton>
+    </PStack>
+  </div>
 </template>
 
 <script lang="ts">
-    import {Vue, Component, Prop} from 'vue-property-decorator';
-    import {
-        ComplexAction,
-        DisableableAction,
-        LoadableAction,
-    } from '@/types';
-    import {PStack} from '@/components/PStack';
-    import {PButtonGroup} from '@/components/PButtonGroup';
-    import {PButton} from '@/components/PButton';
+import {Component, Prop, Vue} from 'vue-property-decorator';
+import {ComplexAction, DisableableAction, LoadableAction} from '@/types';
+import {PStack} from '@/components/PStack';
+import {PButtonGroup} from '@/components/PButtonGroup';
+import {PButton} from '@/components/PButton';
 
-    export interface PageActionsProps {
-        primaryAction?: DisableableAction & LoadableAction;
-        secondaryActions?: ComplexAction[];
-    }
+export interface PageActionsProps {
+  primaryAction?: DisableableAction & LoadableAction;
+  secondaryActions?: ComplexAction[];
+}
 
-    @Component({
-        components: {
-            PStack,
-            PButtonGroup,
-            PButton,
-        },
-    })
-    export default class PPageActions extends Vue {
+@Component({
+  components: {
+    PStack,
+    PButtonGroup,
+    PButton,
+  },
+})
+export default class PPageActions extends Vue {
 
-        @Prop(Object) public primaryAction!: DisableableAction & LoadableAction;
-        @Prop({type: Array, default: () => []}) public secondaryActions!: ComplexAction[];
+  @Prop(Object) public primaryAction!: DisableableAction & LoadableAction;
+  @Prop(Object) public secondaryAction!: ComplexAction;
+  @Prop({type: Array, default: () => []}) public secondaryActions!: ComplexAction[];
 
-        public get distribution() {
-            return this.secondaryActions ? 'equalSpacing' : 'trailing';
-        }
-    }
+  public get distribution() {
+    return this.secondaryActions ? 'equalSpacing' : 'trailing';
+  }
+}
 </script>
