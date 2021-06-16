298a849b2037d001f47211af04c4b721dd42829e
Uttam Rabadiya <uttam.r@planetx.in>
2021-06-16 14:37:53 +0530

    Added resource list documentation in Storybook

---
+++ src/components/PButtonGroup/components/PButtonGroupItem.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <div class="Polaris-ButtonGroup__Item">
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PButtonGroupItem extends Vue {
+}
+</script>
