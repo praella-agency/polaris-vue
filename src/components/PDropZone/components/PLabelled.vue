0385db125f8dd39284a5886a1d02cc0aa2fd1508
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-07-09 19:02:01 +0530

    Working on PLabelled component for PDropZone

---
+++ src/components/PDropZone/components/PLabelled.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from 'vue-property-decorator';
+
+    @Component({})
+    export default class PLabelled extends Vue {
+        /** A unique identifier for the label */
+        @Prop({type: String, default: null}) public id!: string;
+
+        /** Text for the label */
+        @Prop({type: String, default: null}) public label!: string;
+
+        /** Error to display beneath the label */
+        @Prop({type: Boolean, default: false}) public error?: boolean;
+
+        // /** An action */
+        // action?: Action;
+        // /** Additional hint text to display */
+        // helpText?: React.ReactNode;
+        // /** Content to display inside the connected */
+        // children?: React.ReactNode;
+        // /** Visually hide the label */
+        // labelHidden?: boolean;
+        // /** Visual required indicator for the label */
+        // requiredIndicator?: boolean;
+
+    }
+</script>
+
+<style scoped>
+
+</style>
