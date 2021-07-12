574c7197ffbd40e42004f19b16623c2a87189d3a
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-07-12 19:02:14 +0530

    Create Labelled, Label component and Working on Dropzone

---
+++ src/components/PDropZone/components/PLabel.vue
@@ -0,0 +1,52 @@
+<template>
+    <div :class="className">
+        <label
+                :id="id"
+                :for="id"
+                :class="labelClassName"
+        >
+            <slot />
+        </label>
+    </div>
+</template>
+
+<script lang="ts">
+  import {Vue, Component, Prop} from 'vue-property-decorator';
+  import {classNames} from '@/utilities/css';
+
+  @Component({})
+  export default class PLabel extends Vue {
+    /**
+     * A unique identifier for the label
+     */
+    @Prop({type: String, required: true, default: 'polaris-label'}) public id!: string;
+
+    /**
+     * Visually hide the label
+     */
+    @Prop({type: Boolean, default: false}) public hidden!: boolean;
+
+    /**
+     * Visual required indicator for the label
+     */
+    @Prop({type: Boolean, default: false}) public requiredIndicator!: boolean;
+
+    public get className() {
+      return classNames(
+        'Polaris-Label',
+        this.hidden && 'Polaris-Label--hidden',
+      );
+    }
+
+    public get labelClassName() {
+      return classNames(
+        'Polaris-Label__Text',
+        this.requiredIndicator && 'Polaris-Label__RequiredIndicator',
+      )
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/PDropZone/components/PLabelled.vue
+++ src/components/PDropZone/components/PLabelled.vue
@@ -1,33 +1,126 @@
 <template>
-
+    <div :class="className">
+        <div v-if="label" :class="labelClassName">
+            <PLabel
+                    :id="id"
+                    :requiredIndicator="requiredIndicator"
+                    :hidden="false"
+                    v-bind="$attrs"
+            >
+                {{ label }}
+            </PLabel>
+            <div
+                    v-if="action"
+                    :class="actionClassName"
+            >
+                <PButton
+                        plain
+                        v-on="action.onAction ? { click: action.onAction } : {}"
+                >
+                    {{ action.content }}
+                </PButton>
+            </div>
+        </div>
+        <slot/>
+        <div
+                v-if="error && (typeof error !== 'boolean')"
+                :class="errorClass"
+        >
+            <PInlineError :message="error" :fieldID="id"/>
+        </div>
+        <div
+                v-if="helpText"
+                :class="helpTextClassName"
+                :id="helpTextID"
+        >
+            {{ helpText }}
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-    import { Vue, Component, Prop } from 'vue-property-decorator';
+  import {Vue, Component, Prop} from 'vue-property-decorator';
+  import {PLabel} from './index';
+  import {PButton} from '@/components/PButton/index';
+  import {PInlineError} from '@/components/PInlineError/index';
+  import {Action} from '@/types';
+  import {classNames} from '@/utilities/css';
 
-    @Component({})
-    export default class PLabelled extends Vue {
-        /** A unique identifier for the label */
-        @Prop({type: String, default: null}) public id!: string;
-
-        /** Text for the label */
-        @Prop({type: String, default: null}) public label!: string;
-
-        /** Error to display beneath the label */
-        @Prop({type: Boolean, default: false}) public error?: boolean;
-
-        // /** An action */
-        // action?: Action;
-        // /** Additional hint text to display */
-        // helpText?: React.ReactNode;
-        // /** Content to display inside the connected */
-        // children?: React.ReactNode;
-        // /** Visually hide the label */
-        // labelHidden?: boolean;
-        // /** Visual required indicator for the label */
-        // requiredIndicator?: boolean;
-
+  @Component({
+    components: {
+      PLabel, PButton, PInlineError,
     }
+  })
+  export default class PLabelled extends Vue {
+    /**
+     * A unique identifier for the label
+     */
+    @Prop({type: String, default: null}) public id!: string;
+
+    /**
+     * Text for the label
+     */
+    @Prop({type: String, default: null}) public label!: string;
+
+    /**
+     * Error to display beneath the label
+     */
+    @Prop({type: Boolean, default: false}) public error?: boolean;
+
+    /**
+     * An action
+     */
+    @Prop({type: Function}) public action!: Action;
+
+    /**
+     * Additional hint text to display
+     */
+    @Prop({type: String, default: null}) public helpText!: string;
+
+    /**
+     * Visually hide the label
+     */
+    @Prop({type: Boolean, default: false}) public labelHidden!: boolean;
+
+    /**
+     * Visual required indicator for the label
+     */
+    @Prop({type: Boolean, default: false}) public requiredIndicator!: boolean;
+
+    public get className() {
+      return classNames(
+        this.labelHidden && 'Polaris-Labelled--hidden',
+      );
+    }
+
+    public get labelClassName() {
+      return classNames(
+        'Polaris-Labelled__LabelWrapper'
+      );
+    }
+
+    public get actionClassName() {
+      return classNames(
+        'Polaris-Labelled__Action'
+      );
+    }
+
+    public get errorClass() {
+      return classNames(
+        'Polaris-Labelled__Error'
+      );
+    }
+
+    public get helpTextClassName() {
+      return classNames(
+        'Polaris-Labelled__HelpText'
+      );
+    }
+
+    public get helpTextID() {
+      return this.id + 'HelpText';
+    }
+  }
 </script>
 
 <style scoped>
