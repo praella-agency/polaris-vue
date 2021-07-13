e85daff39734870836d23590c010d8f814de632a
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-07-13 19:12:07 +0530

    Working on PDropZone, simplification in components

---
+++ src/components/PDropZone/context.ts
@@ -0,0 +1,59 @@
+export const defaultAllowMultiple = true;
+
+interface DropZoneContextType {
+  disabled: boolean;
+  focused: boolean;
+  measuring: boolean;
+  allowMultiple: boolean;
+  size: string;
+  type: string;
+}
+
+export const Context: DropZoneContextType = {
+  disabled: false,
+  focused: false,
+  size: 'extraLarge',
+  type: 'file',
+  measuring: false,
+  allowMultiple: defaultAllowMultiple,
+};
+
+export function createAllowMultipleKey(allowMultiple: boolean) {
+  return allowMultiple ? 'allowMultiple' : 'single';
+}
+
+export function capitalize(word = '') {
+  const wordLower = word.toLowerCase();
+  return wordLower.charAt(0).toUpperCase() + wordLower.slice(1);
+}
+
+export function fileAccepted(file: File, accept: string | undefined) {
+  return file.type === 'application/x-moz-file' || accepts(file, accept);
+}
+
+export function accepts(file: File, acceptedFiles: string | string[] | undefined) {
+  if (file && acceptedFiles) {
+    const fileName = file.name || '';
+    const mimeType = file.type || '';
+    const baseMimeType = mimeType.replace(/\/.*$/, '');
+    const acceptedFilesArray = Array.isArray(acceptedFiles)
+      ? acceptedFiles
+      : acceptedFiles.split(',');
+
+    return acceptedFilesArray.some((type) => {
+      const validType = type.trim();
+      if (validType.startsWith('.')) {
+        return fileName.toLowerCase().endsWith(validType.toLowerCase());
+      } else if (validType.endsWith('/*')) {
+        // This is something like a image/* mime type
+        return baseMimeType === validType.replace(/\/.*$/, '');
+      }
+      return mimeType === validType;
+    });
+  }
+  return true;
+}
+
+export function isServer() {
+  return typeof window === 'undefined' || typeof document === 'undefined';
+}
