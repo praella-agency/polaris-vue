3d7e8175b0ef6d9cf703af28ddba061e3744934e
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-07-13 16:29:27 +0530

    Added old icons

---
+++ src/assets/shopify-polaris-icons/oldImages/CategoriesMajorMonotone.ts
@@ -0,0 +1,1 @@
+export default `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2 18h16v-8H2v8zm17 2H1a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM17 6H3a1 1 0 0 1 0-2h14a1 1 0 0 1 0 2zm-2-4H5a1 1 0 0 1 0-2h10a1 1 0 0 1 0 2z"/></svg>`;
---
+++ src/assets/shopify-polaris-icons/oldImages/EmailMajorMonotone.ts
@@ -0,0 +1,1 @@
+export default `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2 16V5.414l7.293 7.293a.997.997 0 0 0 1.414 0L18 5.414V16H2zM16.586 4L10 10.586 3.414 4h13.172zM19 2H1a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z"/></svg>`;
