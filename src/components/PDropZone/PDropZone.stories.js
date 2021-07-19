8423391377c9dd5bfeb8de77a6b00abed4d8333b
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-07-19 14:01:47 +0530

    Applied slot support for the DropZone

--- src/components/PDropZone/PDropZone.stories.js
+++ src/components/PDropZone/PDropZone.stories.js
@@ -1,9 +1,5 @@
 import {PDropZone} from './index';
-import {PStack, PStackItem} from '../PStack';
-import {PThumbnail} from '../PThumbnail';
-import {PCaption} from '../PCaption';
 import PFileUpload from './components/PFileUpload';
-import {NoteMinor} from '../../assets/shopify-polaris-icons/index';
 
 export default {
     title: 'Actions / Drop Zone',
@@ -13,50 +9,26 @@ export default {
 const Template = (args, {argTypes}) => ({
     props: Object.keys(argTypes),
     components: {
-        PDropZone, PStack, PThumbnail, PCaption, PFileUpload,PStackItem
+        PDropZone, PFileUpload
     },
     data() {
         return {
-            files: [],
+            images: [],
         };
     },
     template: `
         <PDropZone
             v-bind="$props"
             :handleOnDrop="handleDropZoneDrop"
-            :files="files"
+            :files="images"
         >
-            <PStack
-                v-for="(file, key) in this.files"
-                :key="key"
-                alignment="center"
-                v-slot="uploadedFiles"
-            >
-                <PStackItem>
-                  <PThumbnail
-                      size="small"
-                      :alt="file.name"
-                      :source="['image/gif', 'image/jpeg', 'image/png'].indexOf(file.type) > -1 ? 
-                        createUrl(file) : NoteMinor"
-                  />
-                </PStackItem>
-                <PStackItem>
-                  <div>
-                    {{ file.name }} <PCaption>{{ file.size }} bytes</PCaption>
-                  </div>
-                </PStackItem>
-            </PStack>
-            <PFileUpload v-if="!files.length" />
         </PDropZone>`,
     methods: {
         handleDropZoneDrop(files, acceptedFiles, rejectedFiles) {
             acceptedFiles.map((file) => {
-                this.files.push(file);
+                this.images.push(file);
             })
         },
-        createUrl(file) {
-            return window.URL.createObjectURL(file);
-        }
     },
 })
 
@@ -64,5 +36,4 @@ export const DropZone = Template.bind({});
 
 DropZone.args = {
     allowMultiple: true,
-    disabled: true,
 }
