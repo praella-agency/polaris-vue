838ec36411ed795ace1eedf90a4c2bd8da49db13
anil-hulkapps <anil@hulkapps.com>
2021-07-21 15:02:37 +0530

    PModal slot support

--- src/components/PModal/PModal.stories.js
+++ src/components/PModal/PModal.stories.js
@@ -41,19 +41,6 @@ const Template = (args, {argTypes}) => ({
     template: `
       <div>
           <PModal v-bind="$props"
-                  :secondaryActions= "[
-                      {
-                        content: 'Delete Customer',
-                        'destructive': true,
-                        onAction: handleDeleteAction,
-                      },
-                      {
-                        content:'Cancel',
-                        onAction: () => {
-                          this.is_active = false;
-                        },
-                      },
-                  ]"
                   @close="closeModal"
                   :open="this.is_active"
           >
@@ -92,4 +79,19 @@ Modal.args = {
     },
     title: "Enter Customer Details",
     sectioned: true,
+    secondaryActions: [
+        {
+            content: 'Delete Customer',
+            'destructive': true,
+            onAction: () => {
+                alert('Customer deleted');
+            },
+        },
+        {
+            content:'Cancel',
+            onAction: () => {
+                this.is_active = false;
+            },
+        },
+    ]
 }
