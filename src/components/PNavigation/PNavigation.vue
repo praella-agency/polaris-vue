11ac7e5567432f30e428bd5c98c63c03f544da51
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-07-29 19:02:26 +0530

    Creating Navigation Component

---
+++ src/components/PNavigation/PNavigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <PNavigationContext.Provider>
+
+  </PNavigationContext.Provider>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Inject } from 'vue-property-decorator';
+  import PNavigationContext from './PNavigationContext.vue';
+
+  @Component({
+    components: {
+      PNavigationContext,
+    }
+  })
+  export default class PNavigation extends Vue {
+    @Inject() readonly location!: string;
+    @Inject() readonly onNavigationDismiss!: void;
+    @Inject() readonly withinContentContainer!: boolean;
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PNavigation/PNavigationContext.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Provide } from 'vue-property-decorator';
+
+  interface NavigationContextType {
+    location: string;
+    onNavigationDismiss?(): void;
+    withinContentContainer?: boolean;
+  }
+
+  @Component
+  export default class PNavigationContext extends Vue {
+    @Provide() location!: NavigationContextType['location'];
+    @Provide() onNavigationDismiss!: NavigationContextType['onNavigationDismiss'];
+    @Provide() withinContentContainer!: NavigationContextType['withinContentContainer'];
+  }
+</script>
+
+<style scoped>
+
+</style>
