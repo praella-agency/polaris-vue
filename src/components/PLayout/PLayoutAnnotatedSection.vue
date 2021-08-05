20d7371c67da2160ce5d292d803f269af4c91a86
Divyank <divyank@hulkapps.com>
2021-08-05 15:35:48 +0530

    Fixed title slot

--- src/components/PLayout/PLayoutAnnotatedSection.vue
+++ src/components/PLayout/PLayoutAnnotatedSection.vue
@@ -4,8 +4,10 @@
       <div class="Polaris-Layout__Annotation">
         <PTextContainer>
           <!-- @slot Title for the section  -->
-          <slot name="title" />
-          <PHeading v-if="title">{{ title }}</PHeading>
+          <slot name="title">
+            <PHeading>{{ title }}</PHeading>
+          </slot>
+
           <div v-if="description || $slots.hasOwnProperty('description')" class="Polaris-Layout__AnnotationDescription">
             <!-- @slot Short Description content for the layout  -->
             <slot name="description">
