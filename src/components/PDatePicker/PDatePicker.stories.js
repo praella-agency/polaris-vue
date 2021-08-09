0716d771c439b28b8d5602d31620cd60a625465d
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-08-09 18:01:50 +0530

    Fixed demo prop in DatePicker

--- src/components/PDatePicker/PDatePicker.stories.js
+++ src/components/PDatePicker/PDatePicker.stories.js
@@ -29,7 +29,7 @@ export default {
                 defaultValue: {
                     summary: '{}',
                     detail: `{
-    key: value,
+    text: 'value',
 }`,
                 }
             }
