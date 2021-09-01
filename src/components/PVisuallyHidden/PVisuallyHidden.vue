a9cde2b8a085cf36c149be3503a504a3f6fe4715
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-09-01 17:37:36 +0530

    Added id props for VisuallyHidden. Added VisuallyHidden story and documentation.

--- src/components/PVisuallyHidden/PVisuallyHidden.vue
+++ src/components/PVisuallyHidden/PVisuallyHidden.vue
@@ -1,14 +1,25 @@
 <template>
-  <span class="Polaris-VisuallyHidden">
+  <span class="Polaris-VisuallyHidden" :id="id">
+    <!-- @slot Slot to add content -->
     <slot/>
   </span>
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator';
+  import { Vue, Component, Prop } from 'vue-property-decorator';
 
+  /**
+   * <br/>
+   * <h4 style="font-family: -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue,
+   *  sans-serif;">Use when an element needs to be available to assistive technology (for example, a screen reader) but
+   *  otherwise hidden.</h4>
+   */
   @Component
   export default class PVisuallyHidden extends Vue {
+    /**
+     * Id for the element
+     */
+    @Prop({type: String, default: null}) public id!: string;
   }
 </script>
 
