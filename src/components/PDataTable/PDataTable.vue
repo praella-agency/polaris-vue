2b0dda0bb032a608ed0af4ab33b7260055b0ebd0
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-10-12 14:59:47 +0530

    Added auxiliaryContainer slot to DataTable

--- src/components/PDataTable/PDataTable.vue
+++ src/components/PDataTable/PDataTable.vue
@@ -2,8 +2,12 @@
   <div>
     <PFilter v-if="$slots.hasOwnProperty('filter') || hasFilter" v-bind="$attrs" :resourceName="resource"
              @remove-tag="onRemoveFilter" @input="onFilterInputChanged">
-      <!-- @slot Slot to add a custom Filter content -->
-      <slot name="filter" v-if="$slots.hasOwnProperty('filter')"></slot>
+        <!-- @slot Slot to add a custom Filter content -->
+        <slot name="filter" v-if="$slots.hasOwnProperty('filter')"></slot>
+        <template slot="auxiliaryContainer">
+            <!-- @slot Slot to add auxiliary filters -->
+            <slot name="auxiliaryContainer"/>
+        </template>
     </PFilter>
     <div class="Polaris-DataTable" v-if="rows.length > 0 || $slots.hasOwnProperty('body')">
       <div class="Polaris-DataTable__ScrollContainer">
