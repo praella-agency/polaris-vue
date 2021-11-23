68428cb3ee8d1c5a78247fbbf78177322660ea91
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-11-23 18:49:29 +0530

    Added types js file

---
+++ src/types/types.js
@@ -0,0 +1,19 @@
+export const BaseAction = {
+    id: String,
+    content: String,
+    accessibilityLabel: String,
+    url: String,
+    external: Boolean,
+    onAction: Function,
+}
+export const Action = {
+    ...BaseAction,
+}
+export const DisableableAction = {
+    ...Action,
+    disabled: Boolean
+}
+export const LoadableAction = {
+    ...Action,
+    loading: Boolean
+}
