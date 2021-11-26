6738d729f7c4f7a5a6fd79752d2ecc4cacc79733
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-11-26 17:13:13 +0530

    Converted PRadioButton component to core vue.

--- src/components/PMultiSelect/PMultiSelect.vue
+++ src/components/PMultiSelect/PMultiSelect.vue
@@ -1,243 +1,244 @@
 <template>
-  <div class="">
-    <div class="Polaris-Labelled__LabelWrapper">
-      <div class="Polaris-Label">
-        <label :id="`${id}Label`" :for="id" class="Polaris-Label__Text">
-          {{ label }}
-        </label>
-      </div>
-    </div>
-    <div :class="className">
-      <!--
-       Triggers on searchChange
-       @event searchChange
-       -->
-      <multiselect
-          :id="id"
-          v-model="computedValue"
-          :disabled="disabled"
-          aria-invalid="false"
-          :options="computedOptions"
-          :track-by="valueField"
-          :placeholder="placeholder"
-          :searchable="searchable"
-          :multiple="multiple"
-          :taggable="taggable"
-          :close-on-select="computedMultiple"
-          :clear-on-select="false"
-          :preserve-search="true"
-          :label="textField"
-          @tag="addTag"
-          @search-change="(query) => {$emit('searchChange', query)}"
-      >
-        <template slot="caret">
-          <div class="multiselect__select">
-            <PIcon source="SelectMinor"/>
-          </div>
-        </template>
+    <div class="">
+        <div class="Polaris-Labelled__LabelWrapper">
+            <div class="Polaris-Label">
+                <label :id="`${id}Label`" :for="id" class="Polaris-Label__Text">
+                    {{ label }}
+                </label>
+            </div>
+        </div>
+        <div :class="className">
+            <!--
+             Triggers on searchChange
+             @event searchChange
+             -->
+            <multiselect
+                :id="id"
+                v-model="computedValue"
+                :disabled="disabled"
+                aria-invalid="false"
+                :options="computedOptions"
+                :track-by="valueField"
+                :placeholder="placeholder"
+                :searchable="searchable"
+                :multiple="multiple"
+                :taggable="taggable"
+                :close-on-select="computedMultiple"
+                :clear-on-select="false"
+                :preserve-search="true"
+                :label="textField"
+                @tag="addTag"
+                @search-change="(query) => {$emit('searchChange', query)}"
+            >
+                <template slot="caret">
+                    <div class="multiselect__select">
+                        <PIcon source="SelectMinor"/>
+                    </div>
+                </template>
 
-        <template v-slot:selection="{values, search, remove, isOpen}">
-          <div class="multiselect__tags-wrap" v-show="values && values.length > 0">
-            <template v-for="(option, index) of values" @mousedown.prevent>
-                <PTag :tag='{"value":option[textField], "key":option[valueField]}' removable @remove-tag="remove(option)"/>
-            </template>
-          </div>
-          <template slot="limit"></template>
-        </template>
-      </multiselect>
+                <template v-slot:selection="{values, search, remove, isOpen}">
+                    <div class="multiselect__tags-wrap" v-show="values && values.length > 0">
+                        <template v-for="(option, index) of values" @mousedown.prevent>
+                            <PTag :tag='{"value":option[textField], "key":option[valueField]}' removable
+                                  @remove-tag="remove(option)"/>
+                        </template>
+                    </div>
+                    <template slot="limit"></template>
+                </template>
+            </multiselect>
+        </div>
+        <div class="Polaris-Labelled__HelpText" v-if="helpText">{{ helpText }}</div>
+        <PFieldError v-if="error" :error="error"/>
     </div>
-    <div class="Polaris-Labelled__HelpText" v-if="helpText">{{helpText}}</div>
-    <PFieldError v-if="error" :error="error"/>
-  </div>
 </template>
 
 <script>
-import { classNames } from '../../utilities/css';
-import Multiselect from 'vue-multiselect';
-import { PIcon } from '../../components/PIcon';
-import { PTag } from '../../components/PTag';
-import { PFieldError } from '../../components/PFieldError/index.js';
+    import { classNames } from '../../utilities/css';
+    import Multiselect from 'vue-multiselect';
+    import { PIcon } from '../../components/PIcon';
+    import { PTag } from '../../components/PTag';
+    import { PFieldError } from '../../components/PFieldError/index.js';
 
-export default {
-    name: 'PMultiSelect',
-    components: {
-        PIcon, Multiselect, PTag, PFieldError,
-    },
-    props: {
-        /**
-         * Disable the PMultiSelect.
-         * @values true | false
-         */
-        disabled: {
-            type: Boolean,
-            default: false,
+    export default {
+        name: 'PMultiSelect',
+        components: {
+            PIcon, Multiselect, PTag, PFieldError,
         },
-        /**
-         * Label for the PMultiSelect.
-         */
-        label: {
-            type: String,
-            default: null,
-        },
-        /**
-         * Options of PMultiSelect.
-         */
-        options: {
-            type: Array,
-            required: true,
-            default: () => ([]),
-        },
-        /**
-         * Field name in the `options` array that should be used for the text label
-         */
-        textField: {
-            type: String,
-            default: 'label',
-        },
-        /**
-         * Field name in the `options` array that should be used for the value
-         */
-        valueField: {
-            type: String,
-            default: 'value',
-        },
-        /**
-         * Field name in the `options` array that should be used for the disabled state
-         */
-        disabledField: {
-            type: String,
-            default: 'disabled',
-        },
-        /**
-         * Value for PMultiSelect.
-         */
-        value: {
-            type: [String, Object, Array, Number, Boolean],
-            default: () => ([]),
-        },
-        /**
-         * Disable the searchable options feature.
-         * @values true | false
-         */
-        searchable: {
-            type: Boolean,
-            default: true,
-        },
-        /**
-         * Taggable provides ability to add new user-input value on multiselect.
-         * @values true | false
-         */
-        taggable: {
-            type: Boolean,
-            default: false,
-        },
-        /**
-         * Provide Placeholder.
-         */
-        placeholder: {
-            type: String,
-            default: null,
-        },
-        /**
-         * Help text
-         */
-        helpText: {
-            type: String,
-            default: null,
-        },
-        /**
-         * Validation error
-         */
-        error: {
-            type: String,
-            default: null,
-        },
-        /**
-         * To allow multiple selections
-         * @values true | false
-         */
-        multiple: {
-            type: Boolean,
-            default: true,
-        },
-        /**
-         * Id for the element
-         */
-        id: {
-            type: [String, Number],
-            default: `PolarisMultiSelect${(new Date()).getTime()}`,
-        },
-    },
-    data() {
-        return {
-            selected: this.value,
-        };
-    },
-    computed: {
-        className() {
-            return classNames(
-                'Polaris-Select',
-                this.disabled && 'Polaris-Select--disabled',
-                this.error && 'invalid',
-            );
-        },
-        computedOptions() {
-            const options = [];
-            this.options.map((value) => {
-                if (typeof value === 'object') {
-                    if (value[this.disabledField]) {
-                        value.$isDisabled = value[this.disabledField];
-                    }
-                    options.push(value);
-                } else {
-                    options.push({[this.textField]: value, [this.valueField]: value});
-                }
-            });
-            return options;
-        },
-        computedValue: {
-            get() {
-                return this.selected;
+        props: {
+            /**
+             * Disable the PMultiSelect.
+             * @values true | false
+             */
+            disabled: {
+                type: Boolean,
+                default: false,
             },
-            set(value) {
-                this.selected = value;
-                /**
-                 * Callback when selection is changed
-                 * @property {event}
-                 */
-                this.$emit('change', value);
-                /**
-                 * Callback when input is triggered
-                 * @property {event}
-                 */
-                this.$emit('input', value);
+            /**
+             * Label for the PMultiSelect.
+             */
+            label: {
+                type: String,
+                default: null,
+            },
+            /**
+             * Options of PMultiSelect.
+             */
+            options: {
+                type: Array,
+                required: true,
+                default: () => ([]),
+            },
+            /**
+             * Field name in the `options` array that should be used for the text label
+             */
+            textField: {
+                type: String,
+                default: 'label',
+            },
+            /**
+             * Field name in the `options` array that should be used for the value
+             */
+            valueField: {
+                type: String,
+                default: 'value',
+            },
+            /**
+             * Field name in the `options` array that should be used for the disabled state
+             */
+            disabledField: {
+                type: String,
+                default: 'disabled',
+            },
+            /**
+             * Value for PMultiSelect.
+             */
+            value: {
+                type: [String, Object, Array, Number, Boolean],
+                default: () => ([]),
+            },
+            /**
+             * Disable the searchable options feature.
+             * @values true | false
+             */
+            searchable: {
+                type: Boolean,
+                default: true,
+            },
+            /**
+             * Taggable provides ability to add new user-input value on multiselect.
+             * @values true | false
+             */
+            taggable: {
+                type: Boolean,
+                default: false,
+            },
+            /**
+             * Provide Placeholder.
+             */
+            placeholder: {
+                type: String,
+                default: null,
+            },
+            /**
+             * Help text
+             */
+            helpText: {
+                type: String,
+                default: null,
+            },
+            /**
+             * Validation error
+             */
+            error: {
+                type: String,
+                default: null,
+            },
+            /**
+             * To allow multiple selections
+             * @values true | false
+             */
+            multiple: {
+                type: Boolean,
+                default: true,
+            },
+            /**
+             * Id for the element
+             */
+            id: {
+                type: [String, Number],
+                default: `PolarisMultiSelect${(new Date()).getTime()}`,
             },
         },
-        computedMultiple() {
-            return !this.multiple;
-        },
-    },
-    methods: {
-        addTag(newTag) {
-            const tag = {
-                [this.textField]: newTag,
-                [this.valueField]: newTag,
+        data() {
+            return {
+                selected: this.value,
             };
-            if (this.multiple) {
-                this.selected.push(tag);
-            } else {
-                this.selected = tag;
-            }
-            this.options.push(tag);
-            this.$emit('change', this.selected);
         },
-    },
-    watch: {
-        value(value) {
-            this.selected = value;
+        computed: {
+            className() {
+                return classNames(
+                    'Polaris-Select',
+                    this.disabled && 'Polaris-Select--disabled',
+                    this.error && 'invalid',
+                );
+            },
+            computedOptions() {
+                const options = [];
+                this.options.map((value) => {
+                    if (typeof value === 'object') {
+                        if (value[this.disabledField]) {
+                            value.$isDisabled = value[this.disabledField];
+                        }
+                        options.push(value);
+                    } else {
+                        options.push({[this.textField]: value, [this.valueField]: value});
+                    }
+                });
+                return options;
+            },
+            computedValue: {
+                get() {
+                    return this.selected;
+                },
+                set(value) {
+                    this.selected = value;
+                    /**
+                     * Callback when selection is changed
+                     * @property {event}
+                     */
+                    this.$emit('change', value);
+                    /**
+                     * Callback when input is triggered
+                     * @property {event}
+                     */
+                    this.$emit('input', value);
+                },
+            },
+            computedMultiple() {
+                return !this.multiple;
+            },
         },
-    },
-}
+        methods: {
+            addTag(newTag) {
+                const tag = {
+                    [this.textField]: newTag,
+                    [this.valueField]: newTag,
+                };
+                if (this.multiple) {
+                    this.selected.push(tag);
+                } else {
+                    this.selected = tag;
+                }
+                this.options.push(tag);
+                this.$emit('change', this.selected);
+            },
+        },
+        watch: {
+            value(value) {
+                this.selected = value;
+            },
+        },
+    }
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
--- src/components/PRadioButton/PRadioButton.vue
+++ src/components/PRadioButton/PRadioButton.vue
@@ -1,125 +1,139 @@
 <template>
-  <PChoice :label="label" :label-hidden="labelHidden" :help-text="helpText" :id="id">
-    <!-- @slot Label content -->
-    <slot slot="label" name="label" />
-    <template v-if="$slots.helpText" slot="helpText">
-      <!-- @slot Help text content -->
-      <slot name="helpText"></slot>
-    </template>
+    <PChoice :label="label" :label-hidden="labelHidden" :help-text="helpText" :id="id">
+        <!-- @slot Label content -->
+        <slot slot="label" name="label"/>
+        <template v-if="$slots.helpText" slot="helpText">
+            <!-- @slot Help text content -->
+            <slot name="helpText"></slot>
+        </template>
 
-    <div class="Polaris-RadioButton">
-      <input :id="id"
-             :name="name"
-             :value="value"
-             type="radio"
-             :checked="checked"
-             :disabled="disabled"
-             class="Polaris-RadioButton__Input"
-             @change="onChange"
-             @focus="onFocus"
-             @blur="onBlur"
-             :aria-describedby="helpText ? id+'HelpText' : ''">
+        <div class="Polaris-RadioButton">
+            <input :id="id"
+                   :name="name"
+                   :value="value"
+                   type="radio"
+                   :checked="checked"
+                   :disabled="disabled"
+                   class="Polaris-RadioButton__Input"
+                   @change="onChange"
+                   @focus="onFocus"
+                   @blur="onBlur"
+                   :aria-describedby="helpText ? id+'HelpText' : ''">
 
-      <div class="Polaris-RadioButton__Backdrop"></div>
-      <div class="Polaris-RadioButton__Icon"></div>
-    </div>
+            <div class="Polaris-RadioButton__Backdrop"></div>
+            <div class="Polaris-RadioButton__Icon"></div>
+        </div>
 
-  </PChoice>
+    </PChoice>
 </template>
 
-<script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
-import { classNames } from '@/utilities/css';
-import { PChoice } from '@/components/PChoice/index.js';
-
-/**
- * <br/>
- * <h4 style="font-family: -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue,
- *  sans-serif;">Use radio buttons to present each item in a list of options where merchants must make a single
- *  selection.</h4>
- */
-@Component({
-  components: {
-      PChoice,
-  },
-})
-export default class PRadioButton extends Vue {
-  /**
-   * Id for the radio button.
-   */
-  @Prop({
-    type: [String, Number],
-    default: `PolarisRadioButton${new Date().getUTCMilliseconds()}`,
-    required: true,
-  }) public id!: string | number;
-
-  /**
-   * Label for the radio button.
-   */
-  @Prop({type: String, default: null}) public label!: string;
-
-  /**
-   * Additional text to display.
-   */
-  @Prop({type: String, default: null}) public helpText!: string;
-
-  /**
-   * Name for the radio button.
-   */
-  @Prop({type: String, default: null}) public name!: string;
-
-  /**
-   * Value for the form input.
-   */
-  @Prop({type: [String, Boolean]}) public value!: string|boolean;
-
-  /**
-   * Disable input.
-   * @values true | false
-   */
-  @Prop({type: Boolean, default: false}) public disabled!: boolean;
-
-  /**
-   * Visually hide the label.
-   * @values true | false
-   */
-  @Prop({type: Boolean, default: false}) public labelHidden!: boolean;
-
-  /**
-   * Selected radio butto
-   * @values true | false
-   */
-  @Prop({type: Boolean, default: false}) public checked!: boolean;
-
-  public get className() {
-    return classNames(
-      'Polaris-RadioButton',
-      this.labelHidden && 'Polaris-Choice--labelHidden',
-      this.disabled && 'Polaris-RadioButton--disabled',
-    );
-  }
-  public onChange(e: any) {
-    const target = e.target || e.srcElement;
+<script>
+    import { classNames } from '../../utilities/css';
+    import { PChoice } from '../../components/PChoice';
 
     /**
-     * Callback when radio button is triggered.
-     * @param e
+     * <br/>
+     * <h4 style="font-family: -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue,
+     *  sans-serif;">Use radio buttons to present each item in a list of options where merchants must make a single
+     *  selection.</h4>
      */
-    this.$emit('change', {checked: target.checked, value: target.value});
-  }
+    export default {
+        name: 'PRadioButton',
+        components: {
+            PChoice,
+        },
+        props: {
+            /**
+             * Id for the radio button.
+             */
+            id: {
+                type: [String, Number],
+                default: `PolarisRadioButton${new Date().getUTCMilliseconds()}`,
+                required: true,
+            },
+            /**
+             * Label for the radio button.
+             */
+            label: {
+                type: String,
+                default: null,
+            },
+            /**
+             * Additional text to display.
+             */
+            helpText: {
+                type: String,
+                default: null,
+            },
+            /**
+             * Name for the radio button.
+             */
+            name: {
+                type: String,
+                default: null,
+            },
+            /**
+             * Value for the form input.
+             */
+            value: {
+                type: [String, Boolean],
+            },
+            /**
+             * Disable input.
+             * @values true | false
+             */
+            disabled: {
+                type: Boolean,
+                default: false,
+            },
+            /**
+             * Visually hide the label.
+             * @values true | false
+             */
+            labelHidden: {
+                type: Boolean,
+                default: false,
+            },
+            /**
+             * Selected radio butto
+             * @values true | false
+             */
+            checked: {
+                type: Boolean,
+                default: false,
+            },
+        },
+        computed: {
+            className() {
+                return classNames(
+                    'Polaris-RadioButton',
+                    this.labelHidden && 'Polaris-Choice--labelHidden',
+                    this.disabled && 'Polaris-RadioButton--disabled',
+                );
+            },
+        },
+        methods: {
+            onChange(e) {
+                const target = e.target || e.srcElement;
 
-  public onFocus() {
-    /**
-     * Callback when radio button is focused.
-     */
-    this.$emit('focus');
-  }
-
-  public onBlur() {
-    /**
-     * Callback when focus is removed.
-     */
-    this.$emit('blur');
-  }
-}
+                /**
+                 * Callback when radio button is triggered.
+                 * @param e
+                 */
+                this.$emit('change', {checked: target.checked, value: target.value});
+            },
+            onFocus() {
+                /**
+                 * Callback when radio button is focused.
+                 */
+                this.$emit('focus');
+            },
+            onBlur() {
+                /**
+                 * Callback when focus is removed.
+                 */
+                this.$emit('blur');
+            },
+        },
+    }
 </script>
