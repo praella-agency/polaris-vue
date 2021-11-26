072dfab02e5ef6fe2746607ffecb0d22f983f2f6
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-11-26 19:41:57 +0530

    Converted PPageActions component to core vue.

--- src/components/PPageActions/PPageActions.vue
+++ src/components/PPageActions/PPageActions.vue
@@ -27,18 +27,11 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { ComplexAction, DisableableAction, LoadableAction } from '@/types';
-import { PStack } from '@/components/PStack';
-import { PStackItem } from '@/components/PStack/components/PStackItem';
-import { PButtonGroup } from '@/components/PButtonGroup/index.js';
-import { PButton } from '@/components/PButton/index.js';
-
-export interface PageActionsProps {
-  primaryAction?: DisableableAction & LoadableAction;
-  secondaryActions?: ComplexAction[];
-}
+<script>
+import { PStack } from '../../components/PStack';
+import { PStackItem } from '../../components/PStack/components/PStackItem';
+import { PButtonGroup } from '../../components/PButtonGroup';
+import { PButton } from '../../components/PButton';
 
 /**
  * <br/>
@@ -47,30 +40,38 @@ export interface PageActionsProps {
  *  important because sometimes the primary call to action is hard to access when merchants are at the bottom of a page.
  *  </h4>
  */
-@Component({
-  components: {
-    PStack, PStackItem, PButtonGroup, PButton,
-  },
-})
-export default class PPageActions extends Vue {
-
-  /**
-   * The primary action for the page
-   */
-  @Prop(Object) public primaryAction!: DisableableAction & LoadableAction;
-
-  /**
-   * The secondary action for the page
-   */
-  @Prop(Object) public secondaryAction!: ComplexAction;
-
-  /**
-   * The secondary actions for the page
-   */
-  @Prop({type: Array, default: () => []}) public secondaryActions!: ComplexAction[];
-
-  public get distribution() {
-    return this.secondaryActions ? 'equalSpacing' : 'trailing';
-  }
+export default {
+    name: 'PPageActions',
+    components: {
+        PStack, PStackItem, PButtonGroup, PButton,
+    },
+    props: {
+        /**
+         * The primary action for the page
+         */
+        primaryAction: {
+            type: Object,
+            default: () => ({}),
+        },
+        /**
+         * The secondary action for the page
+         */
+        secondaryAction: {
+            type: Object,
+            default: () => ({}),
+        },
+        /**
+         * The secondary actions for the page
+         */
+        secondaryActions: {
+            type: Array,
+            default: () => ([]),
+        },
+    },
+    computed: {
+        distribution() {
+            return this.secondaryActions ? 'equalSpacing' : 'trailing';
+        },
+    },
 }
 </script>
