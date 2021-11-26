8fb7e2419e3ba892323436892c358356abfc965d
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2021-11-26 18:59:13 +0530

    Converted PLayout component to core vue.

--- src/components/PLayout/PLayout.vue
+++ src/components/PLayout/PLayout.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="Polaris-Layout">
-    <template v-if="sectioned">
-      <PLayoutSection>
-        <!-- @slot The content to display inside the layout -->
-        <slot/>
-      </PLayoutSection>
-    </template>
-    <template v-else>
-      <!-- @slot The content to display inside the layout -->
-      <slot/>
-    </template>
-  </div>
+    <div class="Polaris-Layout">
+        <template v-if="sectioned">
+            <PLayoutSection>
+                <!-- @slot The content to display inside the layout -->
+                <slot/>
+            </PLayoutSection>
+        </template>
+        <template v-else>
+            <!-- @slot The content to display inside the layout -->
+            <slot/>
+        </template>
+    </div>
 </template>
 
-<script lang="ts">
-    import { Component, Vue, Prop } from 'vue-property-decorator';
-    import { PLayoutSection } from '@/components/PLayout/components/PLayoutSection';
+<script>
+    import { PLayoutSection } from '../../components/PLayout/components/PLayoutSection';
 
     /**
      * <br/>
@@ -24,19 +23,21 @@
      *  main configurations: one-column, two-column, and annotated. One and two column layouts can be combined in the
      *  same page. Annotated layouts should be used on their own and only on settings pages.</h4>
      */
-    @Component({
+    export default {
+        name: 'PLayout',
         components: {
             PLayoutSection,
         },
-    })
-    export default class PLayout extends Vue {
-
-      /**
-       * Automatically adds sections to layout.
-       * @values true | false
-       */
-      @Prop({type: Boolean, default: false}) public sectioned!: boolean;
-
+        props: {
+            /**
+             * Automatically adds sections to layout.
+             * @values true | false
+             */
+            sectioned: {
+                type: Boolean,
+                default: false,
+            },
+        },
     }
 </script>
 
--- src/components/PLayout/components/PLayoutSection/PLayoutSection.vue
+++ src/components/PLayout/components/PLayoutSection/PLayoutSection.vue
@@ -5,41 +5,55 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
-import { classNames } from '@/utilities/css';
+<script>
+import { classNames } from '../../../../utilities/css';
 
-@Component
-export default class PLayoutSection extends Vue {
-  /**
-   * Set it true to make it secondary.
-   * @values true | false
-   */
-  @Prop({type: Boolean, default: false}) public secondary!: boolean;
-  /**
-   * Set it true to make it full width.
-   * @values true | false
-   */
-  @Prop({type: Boolean, default: false}) public fullWidth!: boolean;
-  /**
-   * Set it true to make it one half.
-   * @values true | false
-   */
-  @Prop({type: Boolean, default: false}) public oneHalf!: boolean;
-  /**
-   * Set it true to make it one third.
-   * @values true | false
-   */
-  @Prop({type: Boolean, default: false}) public oneThird!: boolean;
-
-  public get className() {
-    return classNames(
-      'Polaris-Layout__Section',
-      this.secondary && 'Polaris-Layout__Section--secondary',
-      this.fullWidth && 'Polaris-Layout__Section--fullWidth',
-      this.oneHalf && 'Polaris-Layout__Section--oneHalf',
-      this.oneThird && 'Polaris-Layout__Section--oneThird',
-    );
-  }
+export default {
+    name: 'PLayoutSection',
+    props: {
+        /**
+         * Set it true to make it secondary.
+         * @values true | false
+         */
+        secondary: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * Set it true to make it full width.
+         * @values true | false
+         */
+        fullWidth: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * Set it true to make it one half.
+         * @values true | false
+         */
+        oneHalf: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * Set it true to make it one third.
+         * @values true | false
+         */
+        oneThird: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    computed: {
+        className() {
+            return classNames(
+                'Polaris-Layout__Section',
+                this.secondary && 'Polaris-Layout__Section--secondary',
+                this.fullWidth && 'Polaris-Layout__Section--fullWidth',
+                this.oneHalf && 'Polaris-Layout__Section--oneHalf',
+                this.oneThird && 'Polaris-Layout__Section--oneThird',
+            );
+        },
+    },
 }
 </script>
