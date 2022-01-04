c804e4897d96ee0d8fdf7a5494c8fac054f47c3f
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2022-01-04 11:05:21 +0530

    Added story for components and fixed docs.

--- src/components/PVerticalDivider/PVerticalDivider.vue
+++ src/components/PVerticalDivider/PVerticalDivider.vue
@@ -3,6 +3,13 @@
 </template>
 
 <script>
+
+    /**
+     * <br/>
+     * <h4 style="font-family: -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue,
+     *  sans-serif;">The component is most often displayed as a vertical rule that is used to separate content
+     *  (or define a change) in a page.</h4>
+     */
     export default {
         name: 'PVerticalDivider',
     }
