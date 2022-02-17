7ad4f18eb8832d5a295b3562e7723631b514e41a
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2022-02-17 12:00:23 +0530

    Added vitebook

---
+++ .vitebook/config.js
@@ -0,0 +1,18 @@
+import { clientPlugin, defineConfig } from '@vitebook/client/node';
+import { vuePlugin } from '@vitebook/vue/node';
+import { defaultThemePlugin } from '@vitebook/theme-default/node';
+
+export default defineConfig({
+  include: ['src/**/*.story.vue'],
+  plugins: [
+    vuePlugin({ appFile: 'App.vue' }),
+    clientPlugin(),
+    defaultThemePlugin(),
+  ],
+  site: {
+    title: '@hulkapps/polaris-vue',
+    description: '',
+    /** @type {(import('@vitebook/theme-default/node').DefaultThemeConfig} */
+    theme: {},
+  },
+});
