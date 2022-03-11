23a1ffe95fa92d329f9d15331dcc8230fec84d56
saurabh-sali-hulkapps <saurabh@hulkapps.com>
2022-03-11 11:12:47 +0530

    Added vue version checker file. Fixed vue 3 support for storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,18 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.js",
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        actions: false,
-      },
-    },
-    "@storybook/addon-actions",
-  ],
+    "stories": [
+        "../src/stories/*.stories.mdx",
+        "../src/stories/*.stories.@(js|jsx|ts|tsx)",
+        "../src/components/PBadge/*.stories.@(js|jsx|ts|tsx)",
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        {
+            name: '@storybook/addon-essentials',
+            options: {
+                actions: false,
+            },
+        },
+        "@storybook/addon-actions",
+    ],
+    "framework": "@storybook/vue3"
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,22 @@
 import '!style-loader!css-loader!sass-loader!../src/scss/main.scss';
 import '!style-loader!css-loader!sass-loader!./styles/docs.scss';
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import PToast from '../src/components/PToast';
-import pLoading from '../src/components/PLoading';
-import { PTooltip } from '../src/components/PTooltip';
+// import Vue from 'vue';
+// import VueRouter from 'vue-router';
+// import PToast from '../src/components/PToast';
+// import pLoading from '../src/components/PLoading';
+// import { PTooltip } from '../src/components/PTooltip';
 
-Vue.use(VueRouter);
-Vue.component('router-link', Vue.component('RouterLink'));
+// Vue.use(VueRouter);
+// Vue.component('router-link', Vue.component('RouterLink'));
 
-Vue.use(PToast);
-Vue.use(pLoading);
-Vue.use(PTooltip);
+// Vue.use(PToast);
+// Vue.use(pLoading);
+// Vue.use(PTooltip);
 
 export const decorators = [(story) => ({
     components: {story},
     template: '<story />',
-    router: new VueRouter()
+    // router: new VueRouter()
 })];
 
 export const parameters = {
